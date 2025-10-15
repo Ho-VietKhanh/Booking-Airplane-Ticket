8475900793c8bf98bf29d1a2413a85c308a61eb2
Ho-VietKhanh <kho20232@gmail.com>
2025-10-15 14:32:30 +0700

    completed Airport, Routes, Flights Entity

---
+++ src/main/java/se196411/booking_ticket/model/FlightsEntity.java
@@ -0,0 +1,35 @@
+package se196411.booking_ticket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="flight")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FlightsEntity {
+    @Id
+    @Column(name = "flight_id", nullable = false)
+    String flightId;
+//    @ManyToOne
+//    @JoinColumn(name = "airplane_id", nullable = false)
+//    private AirplaneEntity airplaneEntity
+    @ManyToOne
+    @JoinColumn(name = "flight_routes_id", nullable = false)
+    private FlightRoutesEntity flightRoute;
+
+    @Column(name = "base_price", nullable = false)
+    private BigDecimal basePrice;
+    @Column(name = "started_time", nullable = false)
+    private LocalDateTime startedTime;
+    @Column(name = "ended_time", nullable = false)
+    private LocalDateTime endedTime;
+    @Column(name = "status", nullable = false)
+    private String status;
+}

8f642107257364b729425a55a5569f402086f4a4
Lam Thanh Y <yltse173000@fpt.edu.vn>
2025-10-27 16:47:46 +0700

    Delete Desktop/Booking-Airplane-Ticket-main 2 directory
    Delete Desktop/Booking-Airplane-Ticket-main 2 directory

--- Desktop/Booking-Airplane-Ticket-main 2/src/main/java/se196411/booking_ticket/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package se196411.booking_ticket.service;
-
-import se196411.booking_ticket.model.RoleEntity;
-import java.util.Optional;
-
-public interface RoleService {
-    Optional<RoleEntity> findByRoleName(String roleName);
-    RoleEntity save(RoleEntity role);
-}

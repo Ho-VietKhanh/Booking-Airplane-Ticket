d321724cff340610307472d9afe1a67524a0b60a
Nguyễn Hoài Anh <megazero205@gmail.com>
2025-11-10 21:29:29 +0700

    added certainly function

--- src/main/java/se196411/booking_ticket/service/MealService.java
+++ src/main/java/se196411/booking_ticket/service/MealService.java
@@ -3,6 +3,7 @@ package se196411.booking_ticket.service;
 import se196411.booking_ticket.model.entity.MealEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MealService {
     MealEntity create(MealEntity meal);
@@ -10,4 +11,6 @@ public interface MealService {
     List<MealEntity> findAll();
     MealEntity update(Integer mealId, MealEntity meal);
     void deleteById(Integer mealId);
+
+    Optional<Object> getAllMeals();
 }
